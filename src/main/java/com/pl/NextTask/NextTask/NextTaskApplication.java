5fb5c39a1d9059f87a76d36f0e943eaecfef844c
Pedro Lucas <plucas2013@gmail.com>
2025-04-29 22:55:16 -0500

    First Commit

---
+++ src/main/java/com/pl/NextTask/NextTask/NextTaskApplication.java
@@ -0,0 +1,13 @@
+package com.pl.NextTask.NextTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pl/NextTask/NextTask/NextTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.NextTask.NextTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
